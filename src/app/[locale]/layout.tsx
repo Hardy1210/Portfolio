3244349404cdeb4da8cd6060d82e74958e51581f
Hardy Lino <harv2222@gmail.com>
2024-11-07 00:01:21 +0100

    portfolio v2.0

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,15 +5,64 @@ import './globals.css'
 import { ThemeProvider } from '../_components/theme-provider'
 //providers parametrado en otro fichero para el idioma
 //import SEO from '../_components/SEO/SEO'
+import { Metadata } from 'next'
 import Head from 'next/head'
 import StructuredData from '../_components/RichSnippets/StructuredData'
 import { Providers } from './providers'
 
-export const metadata = {
-  title: 'Hardy Lino . Frontend Developper',
-  description: 'Frontend developper, React, Next.js, TypeScript',
+export const metadata: Metadata = {
+  title: 'Hardy Lino . Frontend Developer',
+  description: 'Frontend developer, React, Next.js, TypeScript',
+  icons: {
+    icon: '/favicon.ico', // Usa solo favicon.ico aquí
+    apple: '/apple-touch-icon.png', // Puedes agregar apple icon aquí si lo deseas
+  },
+  keywords: [
+    'Frontend developer',
+    'développeur frontend',
+    'desarrollador frontend',
+    'développeur React',
+    'React',
+    'Next.js',
+    'Tailwind CSS',
+    'Sass',
+    'développeur',
+    "développeur d'applications web",
+    'intégrateur web',
+    'création de sites vitrines',
+  ],
+  openGraph: {
+    siteName: 'Hardy Lino',
+    title: 'Hardy Lino Frontend Developer',
+    description: 'Frontend developer, React, Next.js, TypeScript',
+    url: 'https://hardylino.com/',
+    type: 'website',
+    locale: 'fr_FR',
+    images: [
+      {
+        url: '/image/Photos/h-1.webp',
+        width: 1200,
+        height: 630,
+        alt: 'Hardy Lino',
+        type: 'image/webp',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Hardy Lino . Frontend Developer',
+    description: 'Frontend developer, React, Next.js, TypeScript',
+    images: ['/ruta/a/tu_imagen.jpg'],
+  },
+  alternates: {
+    canonical: 'https://hardylino.com/',
+    languages: {
+      es: 'https://hardylino.com/es',
+      en: 'https://hardylino.com/en',
+      fr: 'https://hardylino.com/fr',
+    },
+  },
 }
-
 export default function RootLayout({
   children,
   params,
@@ -26,46 +75,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <Head>
-        <title>{metadata.title}</title>
-
-        <link
-          rel="icon"
-          href="/favicon-16x16.svg"
-          type="image/svg+xml"
-          sizes="32x32"
-        />
-        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-
-        <meta name="description" content={metadata.description} />
-        <meta
-          name="keywords"
-          content="Frontend developer, développeur frontend, desarrollador frontend, développeur React, React, Next.js, Tailwind CSS, Sass, développeur, développeur d'applications web, intégrateur web, création de sites vitrines"
-        />
-        <meta name="author" content="Hardy Lino" />
-        <meta property="og:site_name" content="Hardy Lino" />
-        <link rel="canonical" href="https://hardylino.com/" />
-        <meta property="og:title" content="Hardy Lino Frontend Developer" />
-
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content="/image/Photos/h-1.webp" />
-        <meta property="og:url" content="https://hardylino.com/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:locale" content="fr_FR" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={metadata.title} />
-        <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content="/ruta/a/tu_imagen.jpg" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:image:type" content="image/webp" />
-
-        <link rel="alternate" href="https://hardylino.com/es" hrefLang="es" />
-        <link rel="alternate" href="https://hardylino.com/en" hrefLang="en" />
-        <link rel="alternate" href="https://hardylino.com/fr" hrefLang="fr" />
-
         <StructuredData />
         <script
           dangerouslySetInnerHTML={{
@@ -97,4 +106,40 @@ export default function RootLayout({
       </body>
     </html>
   )
+}
+{
+  /**<title>{metadata.title}</title>
+
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+        <meta name="description" content={metadata.description} />
+        <meta
+          name="keywords"
+          content="Frontend developer, développeur frontend, desarrollador frontend, développeur React, React, Next.js, Tailwind CSS, Sass, développeur, développeur d'applications web, intégrateur web, création de sites vitrines"
+        />
+        <meta name="author" content="Hardy Lino" />
+        <meta property="og:site_name" content="Hardy Lino" />
+        <link rel="canonical" href="https://hardylino.com/" />
+        <meta property="og:title" content="Hardy Lino Frontend Developer" />
+
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content="/image/Photos/h-1.webp" />
+        <meta property="og:url" content="https://hardylino.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="fr_FR" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content="/ruta/a/tu_imagen.jpg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:type" content="image/webp" />
+
+        <link rel="alternate" href="https://hardylino.com/es" hrefLang="es" />
+        <link rel="alternate" href="https://hardylino.com/en" hrefLang="en" />
+        <link rel="alternate" href="https://hardylino.com/fr" hrefLang="fr" /> */
 }
