40e180e17cbe758e7bcc99615cb7c66d56456be4
Hardy Lino <harv2222@gmail.com>
2024-11-07 14:03:23 +0100

    optimisation du projectCards

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -39,7 +39,7 @@ import ProjectCard from '../_components/ProjectCard/ProjectCard'
 import { Section } from '../_components/Section/Section'
 import styles from './page.module.scss'
 
-import { useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import FadeInSection from '../_components/FadeInSection/FadeInSection'
 //skillcards y data
 //import { skillData } from '../skillData/skillData'
@@ -109,9 +109,9 @@ export default function Home() {
     preloadImages(imageUrls)
   }, [imageUrls])
   ///////////////////////
-  {
-    /*este CODIGO SIRVE SOLO SI NO HAY MODALES QUE SE HABRAN POR CADA COMPONENTE
-     //useState para la lista de proyectos que deseen verse
+
+  /*este CODIGO SIRVE SOLO SI NO HAY MODALES QUE SE HABRAN POR CADA COMPONENTE*/
+  //useState para la lista de proyectos que deseen verse
   const [showAll, setShowAll] = useState(false)
   const newItemsRef = useRef<HTMLLIElement>(null)
 
@@ -124,8 +124,6 @@ export default function Home() {
         block: 'start',
       })
     }, 100) // Usamos un pequeño retraso para asegurar que el DOM esté actualizado
-  }
-    */
   }
 
   return (
@@ -195,12 +193,12 @@ export default function Home() {
                   >
                     <span
                       aria-hidden="true"
-                      className="inline-flex flex-col items-center md:items-end md:flex-row gap-3"
+                      className="inline-flex flex-col items-center md:items-end md:flex-row gap-3 "
                     >
-                      <span className="text-7xl font-extrabold drop-shadow-xl text-foreground">
+                      <span className="text-7xl font-extrabold drop-shadow-xl text-foreground whitespace-nowrap">
                         {landingT('hi')}
                       </span>
-                      <span className="text-5xl font-extrabold drop-shadow-xl text-foreground">
+                      <span className=" text-5xl font-extrabold drop-shadow-xl text-foreground">
                         {landingT('myNameIs')}
                       </span>
                     </span>
@@ -250,11 +248,7 @@ export default function Home() {
                     alt="Robot hand dark"
                     width={409}
                     height={425}
-                    priority
-                    className={cn(
-                      styles.img,
-                      ' max-w-xs md:flex hidden dark:block',
-                    )}
+                    className={cn(styles.img, ' max-w-xs md:flex hidden ')}
                   />
                 </div>
               </div>
@@ -738,113 +732,125 @@ export default function Home() {
                     />
                   </FadeInSection>
                 </li>
-                <li className={cn(styles.fadeIn__up, '')}>
-                  <FadeInSection>
-                    <ProjectCard
-                      logoImgSrc="/images/nina/nina-squo.webp"
-                      layerImageSrc="/images/nina/nina-w.webp"
-                      layerImageAlt="logo Nina Carducci"
-                      secondaryImageSrc="/images/nina/nina-squo-1.webp"
-                      secondaryImageAlt="MacBook avec le site Nina Carducci"
-                      headerImageSrc="/images/nina/n-logo.webp"
-                      headerImageAlt="logo Nina Carducci"
-                      icons={[
-                        <HtmlIcon key="html" size={25} />,
-                        <CssIcon key="css" size={25} />,
-                        <PhotoshopIcon key="photoshop" size={25} />,
-                        <LightHouseIcon key="lighthouse" size={25} />,
-                        <WaveIcon key="wave" size={25} />,
-                      ]}
-                      title="Nina Carducci"
-                      description="Optimisation SEO d’un site de photographie, avec ajustement des dimensions des images et restructuration sémantique du code pour un meilleur référencement."
-                      modalDescription="Optimisation SEO pour un site web de photographie. J'ai amélioré les performances du site en optimisant les images, en réduisant leur poids, en ajustant le format et les dimensions pour correspondre au viewport, garantissant ainsi un chargement plus rapide et une meilleure expérience utilisateur. La structure du code a été révisée pour une approche sémantique, facilitant le bon référencement par les moteurs de recherche. J'ai également réalisé un rapport d'optimisation pour comparer les performances avant et après les améliorations, en m'appuyant sur différents audits, tels que Lighthouse pour les performances et WAVE pour l’accessibilité."
-                      modalImgSrc="/images/nina/nina-squo-2.webp"
-                      modalImgAlt="MacBook avec le site Nina Carducci"
-                      modalImgSecondSrc="/images/nina/nina-squo-3.webp"
-                      modalImgSecondAlt="MacBook avec le site Nina Carducci"
-                      technologies={[
-                        'Html',
-                        'Css',
-                        'Photoshop',
-                        'Lighthouse',
-                        'Wave',
-                      ]}
-                      repoLink="https://github.com/Hardy1210/Nina-Carducci"
-                      repoIcon={<GithubIcon size={20} />}
-                      repoLinkWeb="https://hardy1210.github.io/Nina-Carducci/"
-                      repoWebIcon={<WebIcon size={20} />}
-                    />
-                  </FadeInSection>
-                </li>
-                <li className={cn(styles.fadeIn__up, '')}>
-                  <FadeInSection>
-                    <ProjectCard
-                      logoImgSrc="/images/sophie/soph-squo.webp"
-                      layerImageSrc="/images/sophie/so.webp"
-                      layerImageAlt="logo Sophie Bluel"
-                      secondaryImageSrc="/images/sophie/soph-1.webp"
-                      secondaryImageAlt="MacBook Sophie Bluel"
-                      headerImageSrc="/images/sophie/soph.webp"
-                      headerImageAlt="logo Sophie Bluel"
-                      icons={[
-                        <HtmlIcon key="html" size={25} />,
-                        <CssIcon key="css" size={25} />,
-                        <JavascriptIcon key="javascript" size={25} />,
-                        <SwaggerIcon key="swagger" size={25} />,
-                      ]}
-                      title="Sophie Bluel"
-                      description="Sophie Bluel est un site d'architecture intégrant un filtrage dynamique des photos et une interface d'administration pour gérer les projets."
-                      modalDescription="Sophie Bluel est un site web d’architecture pour lequel j’ai intégré un système dynamique de filtrage des photos des projets réalisés par l'architecte. J’ai également développé une page de connexion avec un formulaire interactif, incluant des alertes qui s’affichent en cas d’erreur dans le mot de passe ou l’e-mail. Une page d’administration a été mise en place pour permettre l’ajout et la suppression de photos des travaux de l'architecte via un modal. Pour la communication avec le backend, j'ai utilisé Swagger afin de consulter et tester les endpoints disponibles, assurant ainsi une intégration fluide et fiable."
-                      modalImgSrc="/images/sophie/soph-5.webp"
-                      modalImgAlt="MacBook avec le site Sophie Bluel"
-                      modalImgSecondSrc="/images/sophie/soph-4.webp"
-                      modalImgSecondAlt="Mackbook Sohpie Bluel"
-                      technologies={['Html', 'Css', 'Javascript', 'Swagger']}
-                      repoLink="https://github.com/Hardy1210/Portfolio-architecte"
-                      repoIcon={<GithubIcon size={20} />}
-                      repoLinkWeb=""
-                      repoWebIcon={<WebIcon size={20} />}
-                    />
-                  </FadeInSection>
-                </li>
-                <li className={cn(styles.fadeIn__up, '')}>
-                  <FadeInSection>
-                    <ProjectCard
-                      logoImgSrc="/images/booki/booki-squo.webp"
-                      layerImageSrc="/images/booki/Booki-w.webp"
-                      layerImageAlt="logo Booki"
-                      secondaryImageSrc="/images/booki/bo-1.webp"
-                      secondaryImageAlt="MacBook avec le site Booki"
-                      headerImageSrc="/images/logo_images/ab.webp"
-                      headerImageAlt="Logo Booki"
-                      icons={[
-                        <HtmlIcon key="html" size={25} />,
-                        <CssIcon key="css" size={25} />,
-                      ]}
-                      title="Booki"
-                      description="Booki est l'un de mes premiers projets où j'ai intégré la page d'accueil d'un site de voyages."
-                      modalDescription="Booki est l'une de mes premières intégrations. Ma mission dans ce projet de ma formation était d'intégrer la page d'accueil de ce site web pour une agence de voyages. C'est ainsi que j'ai appris de manière plus approfondie le HTML et le CSS. La page d'accueil est responsive et s'adapte aux téléphones mobiles."
-                      modalImgSrc="/images/booki/bo-4.webp"
-                      modalImgAlt="Mackbook avec le site Booki"
-                      modalImgSecondSrc="/images/booki/bo-3.webp"
-                      modalImgSecondAlt="Mackbook avec le site Booki"
-                      technologies={['Html', 'Css']}
-                      repoLink="https://github.com/Hardy1210/Booki"
-                      repoIcon={<GithubIcon size={20} />}
-                      repoLinkWeb="https://hardy1210.github.io/Booki/"
-                      repoWebIcon={<WebIcon size={20} />}
-                    />
-                  </FadeInSection>
-                </li>
+                {showAll && (
+                  <>
+                    <li ref={newItemsRef} className={cn(styles.fadeIn__up, '')}>
+                      <FadeInSection>
+                        <ProjectCard
+                          logoImgSrc="/images/nina/nina-squo.webp"
+                          layerImageSrc="/images/nina/nina-w.webp"
+                          layerImageAlt="logo Nina Carducci"
+                          secondaryImageSrc="/images/nina/nina-squo-1.webp"
+                          secondaryImageAlt="MacBook avec le site Nina Carducci"
+                          headerImageSrc="/images/nina/n-logo.webp"
+                          headerImageAlt="logo Nina Carducci"
+                          icons={[
+                            <HtmlIcon key="html" size={25} />,
+                            <CssIcon key="css" size={25} />,
+                            <PhotoshopIcon key="photoshop" size={25} />,
+                            <LightHouseIcon key="lighthouse" size={25} />,
+                            <WaveIcon key="wave" size={25} />,
+                          ]}
+                          title="Nina Carducci"
+                          description="Optimisation SEO d’un site de photographie, avec ajustement des dimensions des images et restructuration sémantique du code pour un meilleur référencement."
+                          modalDescription="Optimisation SEO pour un site web de photographie. J'ai amélioré les performances du site en optimisant les images, en réduisant leur poids, en ajustant le format et les dimensions pour correspondre au viewport, garantissant ainsi un chargement plus rapide et une meilleure expérience utilisateur. La structure du code a été révisée pour une approche sémantique, facilitant le bon référencement par les moteurs de recherche. J'ai également réalisé un rapport d'optimisation pour comparer les performances avant et après les améliorations, en m'appuyant sur différents audits, tels que Lighthouse pour les performances et WAVE pour l’accessibilité."
+                          modalImgSrc="/images/nina/nina-squo-2.webp"
+                          modalImgAlt="MacBook avec le site Nina Carducci"
+                          modalImgSecondSrc="/images/nina/nina-squo-3.webp"
+                          modalImgSecondAlt="MacBook avec le site Nina Carducci"
+                          technologies={[
+                            'Html',
+                            'Css',
+                            'Photoshop',
+                            'Lighthouse',
+                            'Wave',
+                          ]}
+                          repoLink="https://github.com/Hardy1210/Nina-Carducci"
+                          repoIcon={<GithubIcon size={20} />}
+                          repoLinkWeb="https://hardy1210.github.io/Nina-Carducci/"
+                          repoWebIcon={<WebIcon size={20} />}
+                        />
+                      </FadeInSection>
+                    </li>
+                    <li className={cn(styles.fadeIn__up, '')}>
+                      <FadeInSection>
+                        <ProjectCard
+                          logoImgSrc="/images/sophie/soph-squo.webp"
+                          layerImageSrc="/images/sophie/so.webp"
+                          layerImageAlt="logo Sophie Bluel"
+                          secondaryImageSrc="/images/sophie/soph-1.webp"
+                          secondaryImageAlt="MacBook Sophie Bluel"
+                          headerImageSrc="/images/sophie/soph.webp"
+                          headerImageAlt="logo Sophie Bluel"
+                          icons={[
+                            <HtmlIcon key="html" size={25} />,
+                            <CssIcon key="css" size={25} />,
+                            <JavascriptIcon key="javascript" size={25} />,
+                            <SwaggerIcon key="swagger" size={25} />,
+                          ]}
+                          title="Sophie Bluel"
+                          description="Sophie Bluel est un site d'architecture intégrant un filtrage dynamique des photos et une interface d'administration pour gérer les projets."
+                          modalDescription="Sophie Bluel est un site web d’architecture pour lequel j’ai intégré un système dynamique de filtrage des photos des projets réalisés par l'architecte. J’ai également développé une page de connexion avec un formulaire interactif, incluant des alertes qui s’affichent en cas d’erreur dans le mot de passe ou l’e-mail. Une page d’administration a été mise en place pour permettre l’ajout et la suppression de photos des travaux de l'architecte via un modal. Pour la communication avec le backend, j'ai utilisé Swagger afin de consulter et tester les endpoints disponibles, assurant ainsi une intégration fluide et fiable."
+                          modalImgSrc="/images/sophie/soph-5.webp"
+                          modalImgAlt="MacBook avec le site Sophie Bluel"
+                          modalImgSecondSrc="/images/sophie/soph-4.webp"
+                          modalImgSecondAlt="Mackbook Sohpie Bluel"
+                          technologies={[
+                            'Html',
+                            'Css',
+                            'Javascript',
+                            'Swagger',
+                          ]}
+                          repoLink="https://github.com/Hardy1210/Portfolio-architecte"
+                          repoIcon={<GithubIcon size={20} />}
+                          repoLinkWeb=""
+                          repoWebIcon={<WebIcon size={20} />}
+                        />
+                      </FadeInSection>
+                    </li>
+                    <li className={cn(styles.fadeIn__up, '')}>
+                      <FadeInSection>
+                        <ProjectCard
+                          logoImgSrc="/images/booki/booki-squo.webp"
+                          layerImageSrc="/images/booki/Booki-w.webp"
+                          layerImageAlt="logo Booki"
+                          secondaryImageSrc="/images/booki/bo-1.webp"
+                          secondaryImageAlt="MacBook avec le site Booki"
+                          headerImageSrc="/images/logo_images/ab.webp"
+                          headerImageAlt="Logo Booki"
+                          icons={[
+                            <HtmlIcon key="html" size={25} />,
+                            <CssIcon key="css" size={25} />,
+                          ]}
+                          title="Booki"
+                          description="Booki est l'un de mes premiers projets où j'ai intégré la page d'accueil d'un site de voyages."
+                          modalDescription="Booki est l'une de mes premières intégrations. Ma mission dans ce projet de ma formation était d'intégrer la page d'accueil de ce site web pour une agence de voyages. C'est ainsi que j'ai appris de manière plus approfondie le HTML et le CSS. La page d'accueil est responsive et s'adapte aux téléphones mobiles."
+                          modalImgSrc="/images/booki/bo-4.webp"
+                          modalImgAlt="Mackbook avec le site Booki"
+                          modalImgSecondSrc="/images/booki/bo-3.webp"
+                          modalImgSecondAlt="Mackbook avec le site Booki"
+                          technologies={['Html', 'Css']}
+                          repoLink="https://github.com/Hardy1210/Booki"
+                          repoIcon={<GithubIcon size={20} />}
+                          repoLinkWeb="https://hardy1210.github.io/Booki/"
+                          repoWebIcon={<WebIcon size={20} />}
+                        />
+                      </FadeInSection>
+                    </li>
+                  </>
+                )}
               </ul>
               {/*button LINK  */}
-              <Link href="/projects" locale="fr">
+              {!showAll && (
                 <div className={cn(styles.buttonProjects__container, '')}>
-                  <button className={cn(styles.button__projects, '')}>
+                  <button
+                    onClick={handleShowMore}
+                    className={cn(styles.button__projects, '')}
+                  >
                     Voir plus
                   </button>
                 </div>
-              </Link>
+              )}
             </div>
           </Section>
 
