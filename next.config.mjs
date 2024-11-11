87a63d55d67ab18f58345cde62ee9e16523f00f9
Hardy Lino <harv2222@gmail.com>
2024-11-11 12:19:48 +0100

    nothing

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configuración de caché para recursos estáticos
-
-  // Configuración de redirección (puedes usar `rewrite` en lugar de `redirect` para evitar cambios en la URL)
-  async rewrites() {
+  async redirects() {
     return [
       {
-        source: '/',
-        destination: '/fr', // Mantén la URL como `/` mientras muestra el contenido de `/fr`
+        source: '/', // Ruta raíz
+        destination: '/fr', // Ruta de idioma predeterminado
+        permanent: true, // `true` hace que la redirección sea permanente (código de estado 308)
       },
     ]
   },
-
-  // Configuración de ISR para regeneración incremental
-  experimental: {
-    isrFlushToDisk: false, // Opción para mejorar el rendimiento en Vercel
-  },
-
-  // Configura la compresión para optimizar el tamaño de los archivos en producción
-  compress: true,
-
-  // Genera mapas de código fuente solo en producción para facilitar la depuración
-  productionBrowserSourceMaps: true,
 }
 
 export default nextConfig
