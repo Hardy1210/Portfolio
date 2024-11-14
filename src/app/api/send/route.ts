afd91292a0e879adc4d73d48b32b2413c603b74a
Hardy Lino <harv2222@gmail.com>
2024-11-14 23:53:11 +0100

    resend test

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -10,7 +10,7 @@ export async function POST(req: Request) {
 
   try {
     const data = await resend.emails.send({
-      from: 'Your Name <harv2222@gmail.com>',
+      from: 'Your Name <mail.hardylino.com>',
       to: [userEmail],
       subject: 'Nuevo mensaje de contacto',
       react: EmailTemplate({ message }),
