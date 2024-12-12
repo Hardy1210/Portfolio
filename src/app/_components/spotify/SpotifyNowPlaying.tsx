29bb6bb53c0d7a9e47fa88179f0aa66d90bf3bc2
Hardy Lino <harv2222@gmail.com>
2024-12-12 13:12:22 +0100

    no commit

--- src/app/_components/spotify/SpotifyNowPlaying.tsx
+++ src/app/_components/spotify/SpotifyNowPlaying.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image' // Importa el componente de Next.js
 import { useEffect, useState } from 'react'
+import { SpotifyIcon } from '../icons/SpotifyIcon'
 
 type CurrentlyPlaying = {
   isPlaying: boolean
@@ -39,13 +40,13 @@ export default function SpotifyNowPlaying() {
   }
 
   return (
-    <div className="relative overflow-hidden  w-60 h-28 space-x-4 bg-[#171717] rounded-lg">
+    <div className="relative p-1 gap-3 flex flew-row items-end overflow-hidden  w-60 h-28 bg-[#171717] rounded-lg">
       <Image //primera imagen que se muestra
         src={currentlyPlaying.albumImageUrl || '/fallback-image.png'} // URL de respaldo
         alt={currentlyPlaying.album || 'Álbum desconocido'}
         width={160} // Tamaño de la imagen
         height={160}
-        className="absolute z-10 border-2 border-neutral-300 -left-14 opacity-85 rounded-full animate-spin w-auto" // Clases adicionales
+        className="absolute z-10 border-2 border-neutral-300 -left-28 top-0 opacity-85 rounded-full animate-spin w-auto" // Clases adicionales
       />
       <Image //segunda imagen que tiene blur y va atras de la principal
         src={currentlyPlaying.albumImageUrl || '/fallback-image.png'} // URL de respaldo
@@ -54,9 +55,27 @@ export default function SpotifyNowPlaying() {
         height={170}
         className="absolute -top-3 blur-2xl -left-12  rounded-full animate-spin w-auto" // Clases adicionales
       />
-      <div className="absolute flex flex-col z-10 bottom-1 right-2  text-neutral-50">
-        <p className="font-bold">{currentlyPlaying.title}</p>
-        <p className="text-sm">
+      <SpotifyIcon
+        width={40}
+        className="z-10 m-0 text-[#FFFF] dark:text-[#00DA5A] "
+      />
+      <div className=" flex flex-col   z-10 bottom-1 right-2  text-neutral-50">
+        <p
+          className="font-bold text-sm "
+          style={{
+            textShadow:
+              '1px 1px 0 #000, -1px 1px 0 #000, 1px -1px 0 #000, -1px -1px 0 #000',
+          }}
+        >
+          {currentlyPlaying.title}
+        </p>
+        <p
+          className="text-xs "
+          style={{
+            textShadow:
+              '1px 1px 0 #000, -1px 1px 0 #000, 1px -1px 0 #000, -1px -1px 0 #000',
+          }}
+        >
           {currentlyPlaying.artist} - {currentlyPlaying.album}
         </p>
       </div>
--- src/app/api/callback/route.ts
+++ src/app/api/callback/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-
+//callback de spotify api
 const clientId = process.env.SPOTIFY_CLIENT_ID!
 const clientSecret = process.env.SPOTIFY_CLIENT_SECRET!
 const redirectUri = process.env.SPOTIFY_REDIRECT_URI!
